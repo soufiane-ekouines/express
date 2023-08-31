81c513b60c5431ac7c10e59f33de58cfb1aec216
soufiane-ekouines <soufianeekouines@gmail.com>
2023-08-31 12:43:03 +0100

    links routes rest api bootcamps

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).send({ success: true, message: 'show all bootcamps'  });
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).send({ success: true, message: 'store bootcamps'  });
+})
+
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).send({ success: true, message: 'show bootcamp by id '+req.params.id  });
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).send({ success: true, message: 'update bootcamps '+req.params.id  });
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).send({ success: true, message: 'delete bootcamps '+req.params.id  });
+})
 const PORT = process.env.PORT || 5000
 
 
