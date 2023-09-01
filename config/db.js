a57a2a1e9ce7cb8b0a76a5003767ba8fb6cb3879
soufiane-ekouines <soufianeekouines@gmail.com>
2023-09-01 14:21:14 +0100

    connect with mongodb

---
+++ config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async ()=>{
+    const conn = await mongoose.connect(process.env.MONGO_URI,{
+        useNewUrlParser:true,
+        // useCreateIndex:true,
+        // useFindAndModify:false,
+        useUnifiedTopology:true
+    });
+    console.log(`mongodb connected:${conn.connection.host}`.cyan.underline.bold)
+}
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import { config } from 'dotenv';
-
-
 import morgan from 'morgan';
-//Route files
+import { connectDB } from './config/db.js';
+import colors from 'colors'
 
-import bootcamps from './routes/bootcamps.js';
 //load env vars
 config({ path: './config/config.env' });
+
+// conncet to db
+connectDB();
+
+//Route files
+import bootcamps from './routes/bootcamps.js';
 
 const app = express();
 
@@ -24,4 +28,10 @@ app.use('/api/v1/bootcamps',bootcamps);
 const PORT = process.env.PORT || 5000
 
 
-app.listen(PORT, console.log("server running in port " + process.env.NODE_ENV + "mode port " + PORT));
+const server = app.listen(PORT, console.log(`server running in port  ${process.env.NODE_ENV} mode port ${PORT}`.yellow.bold));
+
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`error : ${err.message}`.red.bold);
+
+    server.close(()=>process.exit(1));
+});
