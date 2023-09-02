7074019b6fe6687fec069f709c7288ec52f3821e
soufiane-ekouines <soufianeekouines@gmail.com>
2023-09-02 14:59:18 +0100

    update and delete

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,33 +1,41 @@
-import { Bootcamp } from "../models/Bootcamp.js"; 
+import { Bootcamp } from "../models/Bootcamp.js";
 
 //@desc get all bootcamp
 //@route GET /api/v1/bootcamps
 // @access public
-export async function getbootcamp(req,res,next){
+export async function getbootcamp(req, res, next) {
     const bootcamps = await Bootcamp.find();
-    res.status(200).send({ success: true, data: bootcamps  });
+    res.status(200).send({ success: true, count:bootcamps.length ,data: bootcamps });
 }
 
 //@desc get single bootcamp
 //@route GET /api/v1/bootcamps/:id
 // @access public
-export async function getSingleBootcamp(req,res,next){
-    const bootcamps = await Bootcamp.findById(req.params.id);
-    res.status(200).send({ success: true, data: bootcamps  });
+export async function getSingleBootcamp(req, res, next) {
+    try {
+        const bootcamps = await Bootcamp.findById(req.params.id);
+        if (!bootcamps) {
+          return  res.status(400).send({ success: false, data: null });
+        }
+        res.status(200).send({ success: true, data: bootcamps });
+    } catch (err) {
+        res.status(400).send({ success: false, data: null});
+
+    }
 }
 
 //@desc store bootcamp
 //@route POST /api/v1/bootcamps
 // @access private
-export async function  storebootcamp(req,res,next){
+export async function storebootcamp(req, res, next) {
     try {
-        
-     const bootcamp = await Bootcamp.create(req.body);
 
-    res.status(200).send({ success: true, data: bootcamp  });
+        const bootcamp = await Bootcamp.create(req.body);
+
+        res.status(200).send({ success: true, data: bootcamp });
     } catch (err) {
-    res.status(400).send({ success: false, data: null,error:err  });
-        
+        res.status(400).send({ success: false, data: null, error: err });
+
     }
 
 
@@ -36,16 +44,37 @@ export async function  storebootcamp(req,res,next){
 //@desc update bootcamp
 //@route PUt /api/v1/bootcamps/:id
 // @access private
-export function updatebootcamp(req,res,next){
-    res.status(200).send({ success: true, message: 'update bootcamps '+req.params.id  });
+export async function  updatebootcamp(req, res, next) {
+        try {
+        const bootcamps = await Bootcamp.findByIdAndUpdate(req.params.id,req.body,{
+            new:true,
+            runValidators:true
+        });
+        if (!bootcamps) {
+          return  res.status(400).send({ success: false, data: null });
+        }
+        res.status(200).send({ success: true, data: bootcamps });
+    } catch (err) {
+        res.status(400).send({ success: false, data: null,error:err});
+
+    }
 }
 
 
 //@desc delete bootcamp
 //@route delete /api/v1/bootcamps/:id
 // @access private
-export function deletebootcamp(req,res,next){
-    res.status(200).send({ success: true, message: 'delete bootcamps '+req.params.id  });
+export async function  deletebootcamp(req, res, next) {
+        try {
+        const bootcamps = await Bootcamp.findByIdAndDelete(req.params.id);
+        if (!bootcamps) {
+          return  res.status(400).send({ success: false, data: null });
+        }
+        res.status(200).send({ success: true, message: "bootcamps is deleted" });
+    } catch (err) {
+        res.status(400).send({ success: false, data: null});
+
+    }
 
 }
 
